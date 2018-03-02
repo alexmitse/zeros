3ffd0477f891c91984077e64ca8910c8c17391d1
alexmitsevich <alex1998mitsevich@gmail.com>
2018-03-02 18:25:21 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let result = 0;
+    let delitel = 5;
+    for(let i = 0; i<=10;i++){
+        let perem = number / delitel;
+        perem = Math.floor(perem);
+        result += perem;
+        delitel *= 5;
+    }
+
+    return result;
+
 }
